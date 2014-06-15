8ca0a136a94294349f1c096420b2987cce7f66f2
webad012 <webad012@gmail.com>
2014-06-15 08:10:01 +0200

    unlocked levels unavailable for click

--- Assets/Scripts/LevelsController.cs
+++ Assets/Scripts/LevelsController.cs
@@ -4,16 +4,45 @@ using System.Collections;
 public class LevelsController : MonoBehaviour 
 {
     public GameObject[] levels;
-    public float outLeft;
-    public float outRight;
+    public float outLeftX;
+    public float outRightX;
+    public float levelTransitionSpeed = 0.5f;
+    //public UISprite xMark;
+    //public Vector3 xMarkUnusedPos;
 
+    private Vector3 outLeftVector;
+    private Vector3 outRightVector;
     private int currentLevel = 0;
-    private int[] unlockedLevels;
+    private int lastUnlockedLevel;
+    private int lastUnlockedStory;
 
 	// Use this for initialization
 	void Start () 
     {
-        unlockedLevels = PlayerPrefsX.GetIntArray("UnlockedLevels", 0, 1);
+        outLeftVector = new Vector3(outLeftX, 0, 0);
+        outRightVector = new Vector3(outRightX, 0, 0);
+
+        lastUnlockedLevel = PlayerPrefs.GetInt("lastUnlockedLevel", 0);
+        lastUnlockedStory = PlayerPrefs.GetInt("lastUnlockedStory", -1);
+
+        for(int i=0; i<levels.Length; i++)
+        {
+            if(i>lastUnlockedLevel)
+            {
+            //Debug.Log
+                foreach(Transform c in levels[i].transform)
+                {
+                    if(c.name == "xMark")
+                    {
+                        c.gameObject.SetActive(true);
+                    }
+                    else if(c.name == "Button")
+                    {
+                        c.gameObject.collider.enabled = false;
+                    }
+                }
+            }
+        }
 	}
 	
 	// Update is called once per frame
@@ -25,9 +54,19 @@ public class LevelsController : MonoBehaviour
     {
         if (currentLevel > 0)
         {
-            TweenPosition.Begin(levels[currentLevel], 0.5f, new Vector3(outRight, 0, 0));
+            TweenPosition.Begin(levels[currentLevel], levelTransitionSpeed, outRightVector);
             currentLevel--;
-            TweenPosition.Begin(levels[currentLevel], 0.5f, new Vector3(0, 0, 0));
+            TweenPosition.Begin(levels[currentLevel], levelTransitionSpeed, new Vector3(0, 0, 0));
+
+            /*if(currentLevel > lastUnlockedLevel)
+            {
+                xMark.transform.position = outLeftVector;
+                TweenPosition.Begin(xMark.gameObject, levelTransitionSpeed, new Vector3(0, 0, 0));
+            }
+            else
+            {
+                xMark.transform.position = xMarkUnusedPos;
+            }*/
         }
     }
 
@@ -35,9 +74,19 @@ public class LevelsController : MonoBehaviour
     {
         if (currentLevel != levels.Length - 1)
         {
-            TweenPosition.Begin(levels[currentLevel], 0.5f, new Vector3(outLeft, 0, 0));
+            TweenPosition.Begin(levels[currentLevel], levelTransitionSpeed, outLeftVector);
             currentLevel++;
-            TweenPosition.Begin(levels[currentLevel], 0.5f, new Vector3(0, 0, 0));
+            TweenPosition.Begin(levels[currentLevel], levelTransitionSpeed, new Vector3(0, 0, 0));
+
+            /*if(currentLevel > lastUnlockedLevel)
+            {
+                xMark.transform.position = outRightVector;
+                TweenPosition.Begin(xMark.gameObject, levelTransitionSpeed, new Vector3(0, 0, 0));
+            }
+            else
+            {
+                xMark.transform.position = xMarkUnusedPos;
+            }*/
         }
     }
 
